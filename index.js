2ce5e1facc03a274d35215fdcfa1e70d2370fbfd
arfan36 <arfanur36@gmail.com>
2022-09-09 07:01:55 +0600

    38_5-1 Practice Browser api with checklist and interview questions

---
+++ index.js
@@ -0,0 +1,11 @@
+/* 
+1. some middle console log will be logged after 3.5 seconds
+2. take a number from users, using prompt and add 250 to  that number. and then display that number using alert
+3. What is cookies, writing in english at least 5-7 lines
+4. write at least 3 differences between local storage and session storage
+5. differences between cookies, localStorage and sessionStorage
+6. What is event loop in javascript
+    how synchronous and asynchronous code works in JS
+7. Tell me four things that are asynchronous in JS
+
+ */
